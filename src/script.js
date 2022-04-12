cc3ec4fe41015f7af7df354fb8affbe4d7fd3d8d
justEstif <estifanosbeyene21@gmail.com>
2022-04-12 08:19:22 -0400

    organized script to have separate functions for forecast and ui

--- src/script.js
+++ src/script.js
@@ -1,42 +1,57 @@
-// HTML Elements
 const form = document.querySelector(".form");
 const input = document.querySelector("#input");
+const cardTitle = document.querySelector(".card-title");
+const cardBody = document.querySelector(".card-body");
+const cardFooter = document.querySelector(".card-footer");
 
-const API_KEY = "ahFAyjnvqCsKgxdRz4sU4pGtuABnG9Cl";
-// format and validate input
-// TODO: Use form validation
-function passSearched(cityName) {
-  cityName.trim();
-  switch (cityName) {
-    case null || "":
-      return;
-    default:
-      getCityKey(cityName);
-  }
-}
-async function getCityKey(cityInput) {
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${cityInput}`;
-  let response = await fetch(url);
-  if (response.status == 200) {
-    let json = await response.json();
-    if (json.length !== 0) {
-      const key = json[0]["Key"];
-      getCityWeather(key);
+// takes the form input and returns the weather object
+function Forecast(formInput) {
+  const _API_KEY = "ahFAyjnvqCsKgxdRz4sU4pGtuABnG9Cl";
+  async function getCityJson(cityInput) {
+    const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${_API_KEY}&q=${cityInput}`;
+    try {
+      let response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  async function getWeatherJson(cityJson) {
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/${cityJson[0]['Key']}?apikey=${_API_KEY}`
+    try {
+      let response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.log(error);
     }
   }
-  return;
+  async function getData() {
+    const cityJson = await getCityJson(formInput);
+    const weatherJson = await getWeatherJson(cityJson);
+    console.log(weatherJson)
+    return {
+      key: cityJson[0]["Key"],
+      country: cityJson[0]["AdministrativeArea"]["EnglishName"],
+      tempC: weatherJson[0]["Temperature"]["Metric"]["Value"],
+      tempF: weatherJson[0]["Temperature"]["Imperial"]["Value"],
+      text: weatherJson[0]["WeatherText"],
+      icon: weatherJson[0]["WeatherIcon"],
+      day: weatherJson[0]["IsDayTime"],
+      moreInfo: weatherJson[0]["Link"],
+    }
+  }
+  return { getData }
 }
 
-async function getCityWeather(key) {
-  const url = `http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${API_KEY}`;
-  let response = await fetch(url);
-  let json = await response.json();
-  console.log(json);
+function updateUI(weatherObject) {
+  console.log(weatherObject)
+  //functions to set the different parts of the html
 }
-
 // event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  passSearched(input.value);
-  // TODO: Clear input on submit
+  let weather = Forecast(input.value)
+  let weatherObject = weather.getData()
+  weatherObject.then(updateUI)
+  input.value = null;
 });
