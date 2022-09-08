5a32c5ebe9326c4d1e0e5e75ea5df6dbb0146d2b
Estifanos <estifanosbeyene21@gmail.com>
2022-09-08 17:59:13 -0400

    chore: prettier formatting

--- services/openWeather.interface.ts
+++ services/openWeather.interface.ts
@@ -1,27 +1,28 @@
 export interface IGeocodingApi {
-  name: string;
-  lat: number;
-  lon: number;
+  state: string
+  name: string
+  lat: number
+  lon: number
 }
 
 export interface ICurrentWeatherApi {
   main: {
-    temp: number;
-    temp_min: number;
-    temp_max: number;
-    feels_like: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    grnd_level: number;
-  };
+    temp: number
+    temp_min: number
+    temp_max: number
+    feels_like: number
+    pressure: number
+    humidity: number
+    sea_level: number
+    grnd_level: number
+  }
   weather: [
     {
-      id: number;
-      main: string;
-      description: string;
-      icon: string;
+      id: number
+      main: string
+      description: string
+      icon: string
     }
-  ];
-  timezone: number;
+  ]
+  timezone: number
 }
--- services/openWeather.service.ts
+++ services/openWeather.service.ts
@@ -1,82 +1,83 @@
-import ICoordinate from "../interface/coordinate";
-import ICityInfo from "../interface/cityInfo";
-import { IGeocodingApi, ICurrentWeatherApi } from "./openWeather.interface";
-import Converter from "node-temperature-converter";
+import ICoordinate from '../interface/coordinate'
+import ICityInfo from '../interface/cityInfo'
+import { IGeocodingApi, ICurrentWeatherApi } from './openWeather.interface'
+import Converter from 'node-temperature-converter'
 
 class OpenWeather {
-  private apiDomain = "http://api.openweathermap.org";
+  private apiDomain = 'http://api.openweathermap.org'
   private getCityInfo = async (cityName: string) => {
-    const url = `${this.apiDomain}/geo/1.0/direct?q=${cityName}&limit=5&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API}`;
+    const url = `${this.apiDomain}/geo/1.0/direct?q=${cityName}&limit=5&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API}`
     try {
-      let response = await fetch(url);
-      let json: IGeocodingApi[] = await response.json();
-      let { lat: latitude, lon: longitude, name } = json[0];
+      let response = await fetch(url)
+      let json: IGeocodingApi[] = await response.json()
+      let { lat: latitude, lon: longitude, name, state } = json[0]
       const cityInfo: ICityInfo = {
         coordinate: { latitude, longitude },
         name,
-      };
-      return cityInfo;
+        state
+      }
+      return cityInfo
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   private getDifferentTemp = (weather: ICurrentWeatherApi) => {
     const {
-      main: { temp, temp_max, temp_min, feels_like },
-    } = weather;
+      main: { temp, temp_max, temp_min, feels_like }
+    } = weather
 
     const getWeatherObj = (tempK: number) => {
-      const kelvin = new Converter.Kelvin(tempK);
+      const kelvin = new Converter.Kelvin(tempK)
       return {
-        tempC: kelvin.toCelsius().toFixed(2),
-        tempF: kelvin.toFahrenheit().toFixed(2),
-        tempK,
-      };
-    };
+        tempC: parseFloat(kelvin.toCelsius().toFixed(2)),
+        tempF: parseFloat(kelvin.toFahrenheit().toFixed(2)),
+        tempK
+      }
+    }
 
     return {
       currentTemp: getWeatherObj(temp),
       currentTempMax: getWeatherObj(temp_max),
       currentTempMin: getWeatherObj(temp_min),
-      currentTempFeelsLike: getWeatherObj(feels_like),
-    };
-  };
+      currentTempFeelsLike: getWeatherObj(feels_like)
+    }
+  }
 
   private getTimeDate = (timezone: number) => {
-    const localTime = new Date().getTime();
-    const localOffset = new Date().getTimezoneOffset() * 60000;
-    const currentUtcTime = localOffset + localTime;
-    const cityOffset = currentUtcTime + 1000 * timezone;
-    const cityTimeDate = new Date(cityOffset).toTimeString().split(" ");
-    return { time: cityTimeDate[0], date: cityTimeDate[1] };
-  };
+    const localTime = new Date().getTime()
+    const localOffset = new Date().getTimezoneOffset() * 60000
+    const currentUtcTime = localOffset + localTime
+    const cityOffset = currentUtcTime + 1000 * timezone
+    const cityTimeDate = new Date(cityOffset).toTimeString().split(' ')
+    return { time: cityTimeDate[0], date: cityTimeDate[1] }
+  }
 
   private getCityWeather = async (coordinate: ICoordinate) => {
-    const url = `${this.apiDomain}/data/2.5/weather?lat=${coordinate.latitude}&lon=${coordinate.longitude}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API}`;
+    const url = `${this.apiDomain}/data/2.5/weather?lat=${coordinate.latitude}&lon=${coordinate.longitude}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API}`
     try {
-      let response = await fetch(url);
-      let json: ICurrentWeatherApi = await response.json();
+      let response = await fetch(url)
+      let json: ICurrentWeatherApi = await response.json()
       return {
         weather: json.weather[0],
         temp: this.getDifferentTemp(json),
-        timeDate: this.getTimeDate(json.timezone),
-      };
+        timeDate: this.getTimeDate(json.timezone)
+      }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   public getWeather = async (cityName: string) => {
     try {
-      const cityInfo = await this.getCityInfo(cityName);
-      if (typeof cityInfo === "undefined") throw "City not found";
-      const cityWeather = await this.getCityWeather(cityInfo.coordinate);
-      return cityWeather;
+      const cityInfo = await this.getCityInfo(cityName)
+      if (typeof cityInfo === 'undefined') throw 'City not found'
+      const cityWeather = await this.getCityWeather(cityInfo.coordinate)
+      return { ...cityWeather, ...cityInfo }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 }
 
-export default OpenWeather;
+export default OpenWeather
