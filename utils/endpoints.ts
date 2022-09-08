18214a7a187ba004a2bfb5377d869fda4728271f
Estifanos <estifanosbeyene21@gmail.com>
2022-09-08 11:22:16 -0400

    feat: start with nextjs project

---
+++ utils/endpoints.ts
@@ -0,0 +1,16 @@
+const getEnvironmentVariable = (environmentVariable: string): string => {
+  const unvalidatedEnvironmentVariable = process.env[environmentVariable];
+  if (!unvalidatedEnvironmentVariable) {
+    throw new Error(
+      `Couldn't find environment variable: ${environmentVariable}`
+    );
+  } else {
+    return unvalidatedEnvironmentVariable;
+  }
+};
+
+const endpoints = {
+  OPEN_WEATHER_API: getEnvironmentVariable("OPEN_WEATHER_API")
+};
+
+export default endpoints
