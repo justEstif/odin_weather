6055122b35375740263a758e3b159804dfcf8964
Estifanos <estifanosbeyene21@gmail.com>
2022-09-13 06:30:56 -0400

    feat: remove unused meta info, and add charset

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,13 +7,7 @@ const Layout = ({ children }: PropsWithChildren) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Odin Weather" />
-        <meta name="author" content="Estifanos Beyene" />
-        <meta name="author" content="justEstif" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta property="og:site_name" content="Estifanos Beyene" />
-        <meta name="og:title" content="Estifanos Beyene" />
-        <meta property="og:type" content="website" />
+        <meta charSet="utf-8" />
         <title>Odin Weather</title>
       </Head>
 
